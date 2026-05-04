c322783b0cc9d00363c1738132789de28ea8c3e1
Rulindana <tesny2005@gmail.com>
2026-05-04 10:59:24 +0200

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,8 @@ import 'package:aura_ai/main.dart';
 void main() {
   testWidgets('Aura AI loads boot sequence', (WidgetTester tester) async {
     await tester.pumpWidget(const AuraApp());
+    await tester.pump(const Duration(milliseconds: 250));
     expect(find.textContaining('INITIALIZING'), findsOneWidget);
-    await tester.pumpAndSettle();
+    await tester.pump(const Duration(seconds: 2));
   });
 }
